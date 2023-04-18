9e2afb92ebf7213fe36727376893e648efe77b00
Alessandro Ranellucci <alessandro@pintle.it>
2023-04-18 16:33:53 +0200

    Add error message for users trying to use this library with the retired WiFi Shield

--- src/WiFi.h
+++ src/WiFi.h
@@ -3,6 +3,8 @@
 #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || \
     defined(ARDUINO_AVR_UNO_WIFI_REV2) || defined (ARDUINO_NANO_RP2040_CONNECT)
   #include <WiFiNINA.h>
+#elif defined(ARDUINO_AVR_UNO)
+  #error "The WiFi library for the WiFi Shield was renamed to Arduino_WiFiShield. In order to compile your sketch you'll need to install that one and change the #include line in your code."
 #elif defined(ARDUINO_ARCH_ESP8266)
   #include <ESP8266WiFi.h>
 #else
