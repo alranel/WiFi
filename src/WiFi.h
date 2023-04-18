cace603e92c799c7dff10feef054dd213e2a8f09
Alessandro Ranellucci <alessandro@pintle.it>
2023-04-18 12:10:09 +0200

    Do not include ESP32 for now

--- src/WiFi.h
+++ src/WiFi.h
@@ -5,8 +5,6 @@
   #include <WiFiNINA.h>
 #elif defined(ARDUINO_ARCH_ESP8266)
   #include <ESP8266WiFi.h>
-#elif defined(ARDUINO_ARCH_ESP32)
-  #include <ESP32WiFi.h>
 #else
   #error "No WiFi library known for this board."
 #endif
